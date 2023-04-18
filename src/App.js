065eea4d027bc0416c7387367bc4f73d8a646d62
TimotheGonin <gonin.timothe@gmail.com>
2023-04-18 12:19:17 +0200

    footer and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
+			<Jumbotron />
+			<Footer />
 		</Router>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="text-white bg-secondary footer fixed-bottom">
+			<div className="container p-3">
+				<span>&copy; Timothé Gonin</span>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
